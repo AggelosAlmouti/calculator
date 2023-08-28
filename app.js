a3a210706ef191986d58aae768e5e1f7f835e3fe
Aggelos <aggelos.almouti@gmail.com>
2023-08-28 03:42:38 +0300

    solve input bugs solved most if not all input bugs

--- app.js
+++ app.js
@@ -28,45 +28,76 @@ const input = document.querySelector('.display .input');
 const result = document.querySelector('.display .result');
 const buttons = document.querySelectorAll('button');
 let display = a = b = operator = '';
+let floatingPointA = floatingPointB = false;
 
 buttons.forEach(button => {
     button.addEventListener('click', e => {
         //on clear reset variables and display
         if (button.id == 'clear') {
             a = b = operator = display = '';
+            floatingPointA = floatingPointB = false;
             input.innerHTML = '';
             result.innerHTML = '';
         }
 
-        if (button.id == 'backspace' && a != '') {
-            //todo
-        }
-
+        //the operator is used to break up the input string and determine which number to populate
+        //if there is an operator populate 'b' else populate 'a'
+        //check for only one floating point in string a and b accordingly
         if (button.classList == 'number') {
             if (operator) {
-                b += button.innerHTML;
+                if (button.id == 'floating-point') {
+                    if (!floatingPointB) {
+                        b += button.innerHTML;
+                        display += button.innerHTML;
+                        floatingPointB = true;
+                    }
+                } else {
+                    b += button.innerHTML;
+                    display += button.innerHTML;
+                }
+            } else {
+                if (button.id == 'floating-point') {
+                    if (!floatingPointA) {
+                        a += button.innerHTML;
+                        display += button.innerHTML;
+                        floatingPointA = true;
+                    }
+                } else {
+                    a += button.innerHTML;
+                    display += button.innerHTML;
+                }
+            }
+            input.innerHTML = display;
+        }
+
+        if (button.classList == 'operator' && a != '' && b == '') {
+            if (operator && operator != '%') {
+                display = display.slice(0, -1);
+                operator = button.innerHTML;
                 display += button.innerHTML;
             } else {
-                a += button.innerHTML;
+                operator = button.innerHTML;
                 display += button.innerHTML;
             }
             input.innerHTML = display;
         }
 
-        if (button.classList == 'operator' && b == '') {
-            if (operator) {
-                //todo
-            } else {
-                display += button.innerHTML;
-            }
-            operator = button.innerHTML;
-            input.innerHTML = display;
-        }
-
-        if (button.id == 'percentage' && a != '') {
+        if (button.id == 'percentage' && a != '' && b == '') {
             a = operate(a * 1, b * 1, operator);
             result.innerHTML = input.innerHTML = display = a;
             console.log(a);
+        }
+
+        if (button.id == 'backspace' && a != '') {
+            if (b) {
+                a = a.slice(0, -1);
+            } else if (operator) {
+                operator = '';
+            } else if (a) {
+                a = a.slice(0, -1);
+            }
+            display = display.slice(0, -1);
+            input.innerHTML = display;
         }
 
         if (button.id == 'equals' && a != '' && b != '') {
@@ -74,10 +105,7 @@ buttons.forEach(button => {
             result.innerHTML = input.innerHTML = display = a;
             b = operator = '';
         }
-
     })
 });
-
-//------------------todos-----------------------
 //keyboard support
-//error message during 2 consecutive operators
+//backspace bugs
